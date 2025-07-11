56e651e2f4a747169fde982ed2086795e4086a4e
Anwar Ansari <anwarshahid128@gmail.com>
2025-07-11 12:36:54 +0530

    added status updates in local storage based on if task is checked or not. Also, updated settings menu display per task.

--- script.js
+++ script.js
@@ -4,26 +4,6 @@ let taskBox = document.querySelector(".task-box");
 // getting local Storage todo-list
 let todos = JSON.parse(localStorage.getItem("todo-list"));
 
-function showTodo() {
-  let li = "";
-  todos.forEach( function (todo, id) {
-    li +=`<li class="task">
-            <label for="${id}">
-              <input type="checkbox" id="${id}">
-              <p>${todo.name}</p>
-            </label>
-            <div class="settings">
-              <i class="fa-solid fa-ellipsis"></i>
-              <ul class="settings-menu">
-                <li><i class="fa-solid fa-pen-to-square" style="color: #000000;"></i>Edit</li>
-                <li><i class="fa-solid fa-trash-can" style="color: #000000;"></i>Delete</li>
-              </ul>
-            </div>
-          </li>`;
-  });
-  taskBox.innerHTML = li;
-}
-
 taskInput.addEventListener("keyup", function(e) {
   let userTask = taskInput.value.trim();
   if (e.key == "Enter" && userTask) {
@@ -37,3 +17,52 @@ taskInput.addEventListener("keyup", function(e) {
     showTodo();
   }
 })
+
+function showTodo() {
+  let li = "";
+  if(todos) {
+    todos.forEach( function (todo, id) {
+      let isCompleted = todo.status == "completed" ? "checked" : "";
+      li +=`<li class="task">
+              <label for="${id}">
+                <input onClick="updateStatus(this)" type="checkbox" id="${id}" ${isCompleted}>
+                <p class="${isCompleted}">${todo.name}</p>
+              </label>
+              <div class="settings">
+                <i onClick = showMenu(this) class="fa-solid fa-ellipsis"></i>
+                <ul class="settings-menu">
+                  <li><i class="fa-solid fa-pen-to-square" style="color: #000000;"></i>Edit</li>
+                  <li><i class="fa-solid fa-trash-can" style="color: #000000;"></i>Delete</li>
+                </ul>
+              </div>
+            </li>`;
+  });
+  }
+  taskBox.innerHTML = li;
+}
+showTodo();
+
+function updateStatus(selectedTask) {
+  let taskName = selectedTask.parentElement.lastElementChild;
+  let id = selectedTask.id;
+  if (selectedTask.checked) {
+    taskName.classList.add("checked");
+    todos[id].status = "completed";
+  }
+  else {
+    taskName.classList.remove("checked");
+    todos[id].status = "pending";
+  }
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+}
+
+function showMenu(selectedTask) {
+  let settingsMenu = selectedTask.parentElement.lastElementChild;
+  settingsMenu.classList.add("show");
+  document.addEventListener("click", e => {
+    if (e.target.tagName != 'I' || e.target != selectedTask) {
+      settingsMenu.classList.remove("show");
+    }
+  })
+  
+}
